32d1c45e8db1b1523d9ed77f1bc96f3c898cab2e
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-04-08 22:44:20 +0100

    refactor: change story template

--- utils/templates/component.stories.js
+++ utils/templates/component.stories.js
@@ -1,15 +1,21 @@
 module.exports = ({ componentName, componentType, componentFileName }) => ({
   content: `\
 import * as React from 'react';
+import { Story, Meta } from '@storybook/react';
 
 import { ${componentName} } from './${componentFileName}';
 import { ${componentName}Props } from './${componentFileName}.types';
 
 export default {
-    title: '${componentType}s/${componentName}'
+    title: '${componentType}s/${componentName}',
+    component: ${componentName}
+} as Meta;
+
+const Template: Story<${componentName}Props> = args => {
+  return <${componentName} {...args} />
 };
 
-export const Default = (args: ${componentName}Props) => <${componentName} {...args} />;
+export const Default = Template.bind({});
 Default.args = { foo: 'bar' };
 `,
   extension: `.stories.tsx`
