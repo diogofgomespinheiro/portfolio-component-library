1d6bbabe07a89b109717c1bbf38a54ca049ebf0f
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-04-11 17:37:16 +0100

    fix: change circle icon size

--- src/components/icons/circle/circle.tsx
+++ src/components/icons/circle/circle.tsx
@@ -3,8 +3,8 @@ import * as React from 'react';
 function CircleIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width={9}
-      height={9}
+      width={10}
+      height={10}
       viewBox="0 0 9 9"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
